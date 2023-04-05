07d2c2015f4806351c9b0f20fa3001e67d89ced5
Sumit <vermasumit923@gmail.com>
2023-04-05 19:52:36 +0530

    Profile Picture Edit Form styling changed

--- src/components/ProfilePictureEditForm.js
+++ src/components/ProfilePictureEditForm.js
@@ -7,12 +7,22 @@ function ProfilePictureEditForm(props) {
         <div className="App">
         <header className="App-header">
             <form action={`http://localhost:8000/edit/profilePicture/${props.id}`} method="POST" encType='multipart/form-data'>
+            <Container className="content m-3">
+     <div className="row card box shadow-lg bg-body rounded">
+       <div className="col-sm-12">
+              <div className="row">
             <div className="form-group col-md-4">
                 <label>Profile Picture: </label>
                 <input type="file" name="profilePicture" className="form-control" placeholder="Enter profile picture" />
                </div>
             <button type="submit" class="btn btn-warning btn-lg m-3">Submit</button>
+            </div>
+            </div>
+            </div>
+            </Container>
             </form>
+            
+
             <br></br>
         </header>
     </div>
