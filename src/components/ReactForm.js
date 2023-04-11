ab27239f750967d1b2b5da590fe137c027c100e1
Sumit <vermasumit923@gmail.com>
2023-04-11 21:37:41 +0530

    removed login sigup logic

--- src/components/ReactForm.js
+++ src/components/ReactForm.js
@@ -6,14 +6,13 @@ import MyProjects from './myProjects';
 import MySkills from './mySkills';
 import MyAchievements from './myAchievements';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import useUser from '../hooks/useUser';
+
 
 function ReactForm() {
-  const {user, isLoading} = useUser();
   return (
     <div className="App">
       <header className="App-header">
-        {user && <form action="http://localhost:8000/portfolio/insert" method="POST" encType='multipart/form-data' novalidate class="validated-form">
+        <form action="http://localhost:8000/portfolio/insert" method="POST" encType='multipart/form-data' novalidate class="validated-form">
           <FirstLayer name='' telephone='' description='' instagram='' linkedIn='' email='' profilePicture={{url: null, filename: null}} mainDesignations={['']} />
           <br></br>
           <MyEducation data={[{institutionName: "", place: "", year: "", aggregate: "", coursePursuied: ""}]}/>
@@ -27,7 +26,7 @@ function ReactForm() {
           <MyAchievements data={[""]}/>
           <button type="submit" class="btn btn-warning btn-lg m-3">Submit</button>
           
-        </form>}
+        </form>
         <br></br>
       </header>
     </div>
